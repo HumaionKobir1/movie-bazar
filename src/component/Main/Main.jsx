f92b04413d898d0e522eedc0c6833cfdee604e4c
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-04-02 23:38:05 +0600

    Create component and added nav file

---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+
+const Main = () => {
+    return (
+        <div>
+            <Nav></Nav>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/component/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <h1>Humaion Kobir</h1>
+        </nav>
+    );
+};
+
+export default Nav;
